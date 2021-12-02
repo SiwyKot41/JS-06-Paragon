ebd33a078c9b5fff2c9d997db3c4d80245dfd4b9
SiwyKot41 <Frutkowski80@gmail.com>
2021-12-02 10:23:33 +0100

    Add new product parameter, fix errors

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-const productName = document.getElementById('product-name')
-const amount = document.getElementById('amount')
-const price = document.getElementById('price')
 const form = document.getElementById('add-product')
 let products = []
 
-
 form.addEventListener('submit', (e) => {
+    const productName = document.getElementById('product-name')
+    const amount = document.getElementById('amount')
+    const price = document.getElementById('price')
     let product = {
         name: productName.value,
         amount: amount.value,
-        price: price.value
+        price: price.value,
+        summaryPrice: price.value * amount.value
     }
     products.push(product)
+    console.log(products)
+    e.preventDefault()
 })
-
-// console.log(products)
-
-function writeProducts() {
-}
